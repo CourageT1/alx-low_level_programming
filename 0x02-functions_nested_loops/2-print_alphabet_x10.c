aa49b559bb685be078f6e155e2072e0711b2829e
CourageT1 <124376251+CourageT1@users.noreply.github.com>
2023-02-23 05:10:00 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,13 @@
 */
 void print_alphabet_x10(void)
 {
-char ch;
 int i;
-i = 10;
-while (i < 10)
+int j;
+for (j = 1; i <= 10; i++)
 {
-ch = 'a';
-while (ch <= 'z')
+for (j = 97; j <= 122; j++)
 {
-_putchar(ch);
-ch++;
+_putchar(j);
 }
 _putchar('\n');
-i++;
-}
 }
