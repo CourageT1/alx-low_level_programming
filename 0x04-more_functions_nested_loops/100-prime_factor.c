52a6503c16b82c1ed4eabef77e71ec5ad620dd8f
CourageT1 <tcgwizhi@gmail.com>
2023-02-23 17:27:07 -0800

    Task11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+/**
+ * main - Entry point
+ */
+int main(void)
+{
+long x, maxf;
+long number = 612852475143;
+doable square = sqrt(number);
+
+for (x = 1; x <= square; xtt)
+{
+if (number % x = 0)
+{
+maxf = number / x;
+}
+}
+printf("%ld\n", maxf);
+return (0);
+}
