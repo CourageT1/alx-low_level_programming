0cff1db2844eafa70d9da9d33ad55154a1d7ec24
CourageT1 <124376251+CourageT1@users.noreply.github.com>
2023-02-24 21:00:42 +0200

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,6 +5,7 @@
  * @a: First integer
  * @b: Second integer
  * @c: third integer
+ * Description: Fix the code so that it correctly prints out the largest of three numbers, no matter the case
  * Return: largest number
  */
 int largest_number(int a, int b, int c)
