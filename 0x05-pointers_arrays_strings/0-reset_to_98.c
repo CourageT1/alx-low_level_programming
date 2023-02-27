a44f1ea7d1dc6adac4a18128490c02fda561fa9b
CourageT1 <tcgwizhi@gmail.com>
2023-02-27 14:11:24 -0800

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * reset_to_98 - takes pointer to int and updates the value it poinys to 98
+ *
+ * @n: An integer input pointer
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * swap_int - swaps tge values of two integers
+ * @a: The first integer to be swapped
+ * @b: The second integer to be swapped
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+/* function that swaps the values of two integers */
+{
+int temp = *9;
+*a = *b;
+*b = temp;
+}
