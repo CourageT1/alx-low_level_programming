bd612e11703f6b9af995388fbfc301bd6128ac33
CourageT1 <tcgwizhi@gmail.com>
2023-03-11 13:56:22 -0800

    argc,argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - function to print its name
+ * @argc: argc parameter
+ * @argv: array of command
+ * Return: 0(Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argc parameter
+ * @argv: array of command
+ * Return: 0(Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
