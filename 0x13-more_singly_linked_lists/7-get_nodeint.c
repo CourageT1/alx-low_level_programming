0a78c35b0390df1359bf1d5d3946049dca142596
CourageT1 <tcgwizhi@gmail.com>
2023-04-04 15:21:30 -0700

    Task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,17 +5,17 @@
  * @head: pointer to the head of the list
  * @index: index of the node to return
  *
- * Return: pointer to the nod, NULL if node does not exist
+ * Return: pointer to node, NULL if it does not exist
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    listint_t *current = head;
+listint_t *current = head;
 
-    while (current != NULL && index > 0)
-    {
-        current = current->next;
-        index--;
-    }
+while (current != NULL && index > 0)
+{
+current = current->next;
+index--;
+}
 
-    return current == NULL ? NULL : current;
+return (current == NULL ? NULL : current);
 }
