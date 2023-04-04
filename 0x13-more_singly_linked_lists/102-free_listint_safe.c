2be2e44df3a4660b5780356c74a06e17ac028089
CourageT1 <124376251+CourageT1@users.noreply.github.com>
2023-04-04 23:36:02 +0200

    Create 102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - Frees a linked list of integers
+ * @h: Pointer to pointer to the head of the list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+    listint_t *current, *tmp;
+    size_t count = 0;
+
+    if (h == NULL)
+        return (0);
+
+    current = *h;
+    *h = NULL;
+
+    while (current != NULL)
+    {
+        count++;
+        if (current <= current->next)
+        {
+            free(current);
+            break;
+        }
+        tmp = current;
+        current = current->next;
+        free(tmp);
+    }
+
+    return (count);
+}
