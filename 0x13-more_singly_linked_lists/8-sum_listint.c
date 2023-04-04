7820fa2401c0015c2f3303ef7dd2fec7e12c2238
CourageT1 <tcgwizhi@gmail.com>
2023-04-04 15:46:53 -0700

    Task 8and9

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,9 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - calcultes the sum of all data
+ * @head: first node
+ * Return: sum
+ */
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    while (head != NULL) {
-        sum += head->n;
-        head = head->next;
-    }
-    return sum;
+int sum = 0;
+listint_t *head = current;
+
+while (head != NULL)
+{
+sum += head->n;
+head = head->next;
+}
+return (sum);
 }
