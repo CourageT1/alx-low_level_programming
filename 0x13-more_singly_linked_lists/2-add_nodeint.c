5d7805401eab0d6c20dc8a92b326a467ff13ffec
CourageT1 <tcgwizhi@gmail.com>
2023-04-04 06:28:32 -0700

    Task2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,17 +3,21 @@
 /**
  * add_nodeint - adds a new node
  * @head: pointer to the first node
- * @data: data to insert in that new node
+ * @n: data to insert in that new node
  *
- * Return: pointer to a new node
+ * Return: pointer to a new node,NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, cons int data);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
+listint_t *new_node;
 new_node = malloc(sizeof(listint_t));
+
 if (new_node == NULL)
 return (NULL);
-new_node->data = data;
+
+new_node->n = n;
 new_node->next = *head;
 *head = new_node;
-return (new);
+
+return (new_node);
 }
