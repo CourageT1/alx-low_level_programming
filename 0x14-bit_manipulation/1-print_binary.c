da2d472ae92bd6cc0ebb4f0f6d7ac960339054f9
CourageT1 <tcgwizhi@gmail.com>
2023-04-09 11:35:29 -0700

    C

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,10 +7,10 @@ void print_binary(unsigned long int n)
 {
 int value = sizeof(unsigned long int) * 8;
 
-for (int i = value - 1; 1 >= 0; i--)
+for (int i = value - i; i >= 0; i--)
 {
 int bit = (n >> i) & 1;
 printf("%d", bit);
 }
-print("\n");
+printf("\n");
 }
