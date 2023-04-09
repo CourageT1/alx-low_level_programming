bde861155716939d611e4ddb98eafe48d2229235
CourageT1 <tcgwizhi@gmail.com>
2023-04-09 01:37:15 -0700

    advanced

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks machine
+ *
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+int i = 1;
+unsigned int i = 1;
+{
+if (*c)
+return (1);
+}
+else
+return (0);
+}
