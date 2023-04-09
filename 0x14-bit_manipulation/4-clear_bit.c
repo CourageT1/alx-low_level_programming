90696a8b6e16ee6ecde25c13fad1a3b7c67cc482
CourageT1 <tcgwizhi@gmail.com>
2023-04-09 01:30:39 -0700

    task4&5

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value
+ * @n: pointer to the number to change
+ * @index: bit to clear
+ * Return: 1 (success), -1 (failure)
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index > 63)
+	return (-1);
+*n &= ~(null << index);
+return (1);
+}
