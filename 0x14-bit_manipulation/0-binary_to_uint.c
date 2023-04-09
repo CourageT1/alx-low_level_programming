c8854c8f90489c26c637ec60942a3dfb91c1b268
CourageT1 <124376251+CourageT1@users.noreply.github.com>
2023-04-09 20:20:00 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts a binary number
+ * binary_to_uint - converts a binary number
  * @b: string containing the number
  *
  * Return: converted number
@@ -10,14 +10,12 @@ unsigned int binary_to_uint(const char *b)
 {
 if (!b)
 return (0);
-}
-int i;
+
+int i = 0;
 unsigned int value = 0;
 
 while (b[i] != '\0')
 {
-value = value << 1;
-if (b[i] == '1')
 value = value << 1;
 if (b[i] == 1)
 value = value | 1;
