16da1a44e552f9f7701f86049faa19e4dd34e12f
CourageT1 <124376251+CourageT1@users.noreply.github.com>
2023-04-10 20:10:35 +0200

    Create courage.c

---
+++ 0x15-file_io/courage.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 1024
+
+void print_error(int code, const char* message) {
+    dprintf(STDERR_FILENO, "Error: %s\n", message);
+    exit(code);
+}
+
+void copy_file(const char* file_from, const char* file_to) {
+    int fd_from, fd_to;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read, bytes_written;
+
+    // Open file_from for reading
+    fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1) {
+        print_error(98, strerror(errno));
+    }
+
+    // Open file_to for writing, truncate if it already exists
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (fd_to == -1) {
+        print_error(99, strerror(errno));
+    }
+
+    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
+        bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written == -1) {
+            print_error(99, strerror(errno));
+        }
+    }
+
+    if (bytes_read == -1) {
+        print_error(98, strerror(errno));
+    }
+    if (close(fd_from) == -1) {
+        print_error(100, strerror(errno));
+    }
+    if (close(fd_to) == -1) {
+        print_error(100, strerror(errno));
+    }
+}
+
+int main(int argc, char* argv[]) {
+ 
+    if (argc != 3) {
+        dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+        exit(97);
+    }
+    copy_file(argv[1], argv[2]);
+
+    return 0;
+}
