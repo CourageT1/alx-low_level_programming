690d650011a528086a38cc54084425dc78d7c663
CourageT1 <tcgwizhi@gmail.com>
2023-08-27 12:46:54 -0700

    hotfix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an island perimeter measuring function."""
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in grid.
@@ -10,18 +11,18 @@ def island_perimeter(grid):
     """
     rows = len(grid)
     cols = len(grid[0]) if rows > 0 else 0
-    
+
     perimeter = 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Each land cell contributes 4 to the perimeter
-                
+                perimeter += 4
+
                 # Check adjacent cells
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Deduct 2 for the shared edge with the upper cell
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Deduct 2 for the shared edge with the left cell
-    
+                    perimeter -= 2
+
     return perimeter
